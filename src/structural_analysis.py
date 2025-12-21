1a9da775dfcec1ded8b4a0a81cc210d5c9b2cf6a
Sharesth <sharesth444yt@gmail.com>
2025-12-21 23:34:06 +0530

    written code in analysis file

---
+++ src/structural_analysis.py
@@ -0,0 +1,10 @@
+"""
+Structural break detection for systemic risk metrics.
+"""
+
+import ruptures as rpt
+
+
+def detect_structural_breaks(series, penalty=10):
+    algo = rpt.Pelt(model="rbf").fit(series.values)
+    return algo.predict(pen=penalty)
